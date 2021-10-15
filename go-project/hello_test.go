ec20d59d9bf8004cc886798b4885ac75619a5484
Sally Lehman <slehman@ripple.com>
2021-10-15 12:24:38 -0700

    test refactor

--- go-project/hello_test.go
+++ go-project/hello_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
